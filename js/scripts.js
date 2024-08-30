a9324ca3a4f3eb312bcb08d61e0334bdc17ce6e9
jeje0730 <KTC24A31G0018@edu.kyoto-tech.ac.jp>
2024-08-30 16:32:26 +0900

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('自己紹介サイトへようこそ！');
 });
+function toggleMenu() {
+    document.querySelector('.hamburger').classList.toggle('change');
+    document.querySelector('.menu').classList.toggle('show');
+}
